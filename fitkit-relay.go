cc0ab4d52323f6815d55a25b751fa1a796896035
Jan Chaloupka <osobni@janchaloupka.cz>
2019-11-05 16:54:36 +0100

    Zpracování příkazů

--- fitkit-relay.go
+++ fitkit-relay.go
@@ -1,10 +1,26 @@
 package main;
 
-import "go.bug.st/serial.v1"
-import "log"
-import "fmt"
+import(
+	"go.bug.st/serial.v1"
+	"fmt"
+	"log"
+	"flag"
+) 
 
 func main() {
+	flagListPorts := flag.Bool("listports", false, "List all available serial ports")
+	flagOpen := flag.String("open", "", "Open serial port communication with FITkit")
+
+	flag.Parse()
+
+	if *flagListPorts{
+		listPorts()
+	}else if *flagOpen != "" {
+		fmt.Println("Connecting to: " + *flagOpen)
+	}
+}
+
+func listPorts(){
 	ports, err := serial.GetPortsList()
 	
 	if err != nil {
